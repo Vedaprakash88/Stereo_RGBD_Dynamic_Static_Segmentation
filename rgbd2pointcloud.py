7b975239b5a3e078ff2b7a3e4e729af204aa2fea
Vedaprakash88 <vedachintha@gmail.com>
2024-01-22 19:29:23 +0100

    update2

--- rgbd2pointcloud.py
+++ rgbd2pointcloud.py
@@ -1,48 +1,48 @@
 import os
-import numpy as np
+import pandas as pd
 import open3d as o3d
 from tqdm import tqdm
-# Directories
-rgb_dir = "D:\\10. SRH_Academia\\1. All_Notes\\4. Thesis\\5. WIP\\Data\\KITTI\\archive\\RGB_images"
-depth_dir = "D:\\10. SRH_Academia\\1. All_Notes\\4. Thesis\\5. WIP\\Data\\KITTI\\archive\\Depth_images"
-ply_dir = "D:\\10. SRH_Academia\\1. All_Notes\\4. Thesis\\5. WIP\\Data\\KITTI\\archive\\PLY"
 
-# Subdirectories
-sub_dirs = ["training", "testing"]
+class Rgb23D:
+    @staticmethod
+    def get_paths():
+        # Directories
+        rgb_dir = "D:\\10. SRH_Academia\\1. All_Notes\\4. Thesis\\5. WIP\\Data\\KITTI_Motion\\RGB_images"
+        depth_dir = "D:\\10. SRH_Academia\\1. All_Notes\\4. Thesis\\5. WIP\\Data\\KITTI_Motion\\Depth_images"
+        ply_dir = "D:\\10. SRH_Academia\\1. All_Notes\\4. Thesis\\5. WIP\\Data\\KITTI_Motion\\PLY"
+        error_folder = "D:\\10. SRH_Academia\\1. All_Notes\\4. Thesis\\5. WIP\\Data\\KITTI_Motion\\Error_Handling"
+        error_df = pd.DataFrame()
+        sub_dirs = ["testing", "training"]
+        return rgb_dir, depth_dir, ply_dir, sub_dirs, error_folder, error_df
 
-def visualize_ply(file_path):
-    # Load the .ply file
-    ply = o3d.io.read_point_cloud(file_path)
+    @staticmethod
+    def visualize_ply(file_path):
+        # Load the .ply file
+        ply = o3d.io.read_point_cloud(file_path)
 
-    # Visualize the point cloud
-    o3d.visualization.draw_geometries([ply])
+        # Visualize the point cloud
+        o3d.visualization.draw_geometries([ply])
 
-
-for sub_dir in sub_dirs:
-    # Get list of files in each directory
-    rgb_files = os.listdir(os.path.join(rgb_dir, sub_dir))
-    depth_files = os.listdir(os.path.join(depth_dir, sub_dir))
-
-    # Find common files in both directories
-    common_files = list(set(rgb_files) & set(depth_files))
-
-    for file in tqdm(common_files):
-        # Load RGB image
+    @staticmethod
+    def read_images(rgb_dir, depth_dir, sub_dir, file):
+        # Get list of files in each directory
         rgb_image = o3d.io.read_image(os.path.join(rgb_dir, sub_dir, file))
 
         # Load depth image
         depth_image = o3d.io.read_image(os.path.join(depth_dir, sub_dir, file))
 
-        # Create RGBD image
-        rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(rgb_image, depth_image, convert_rgb_to_intensity=False)
+        return rgb_image, depth_image
+
+    @staticmethod
+    def create_point_cloud(rgb_image, depth_image, ply_dir, sub_dir, file):
+        rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(rgb_image, depth_image,
+                                                                        convert_rgb_to_intensity=False)
 
         # Create point cloud
         pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image,
                                                              o3d.camera.PinholeCameraIntrinsic(
                                                                  o3d.camera.PinholeCameraIntrinsicParameters.
                                                                  PrimeSenseDefault))
-
         # Save point cloud
         o3d.io.write_point_cloud(os.path.join(ply_dir, sub_dir, file.replace('.png', '.ply')), pcd)
-
-visualize_ply("D:\\10. SRH_Academia\\1. All_Notes\\4. Thesis\\5. WIP\\Data\\KITTI_Motion\\PLY\\training\\000000_00.ply")
+        return f'{file} point cloud created'
